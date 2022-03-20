bc36075e5929f4744e83fd1f689311f7e19e4c76
Ignasius Wahyudi <igniz87@gmail.com>
2022-03-20 19:24:54 +0700

    :ambulance: fix: add default suffix idr

--- index.js
+++ index.js
@@ -2,29 +2,36 @@ import alfy from "alfy";
 import dayjs from "dayjs";
 import formatThousands from "format-thousands";
 
-const [ticker, amount = 1] = alfy.input.split(" ");
+let [ticker, amount = 1] = alfy.input.trim().split(" ");
 
 const data = await alfy.fetch("https://indodax.com/api/ticker_all", {
-	maxAge: 1000 * 60,
+  maxAge: 1000 * 60,
 });
 
-const item = data.tickers[ticker.replace("idr", "_idr")] || undefined;
+let item;
+if (ticker.includes('idr')) {
+  item = data.tickers[ticker.replace("idr", "_idr")] || undefined;
+}
+else {
+  item = data.tickers[`${ticker}_idr`] || undefined;
+}
+
 
 if (item) {
-	const date = dayjs(item.server_time * 1000).format("DD MMM YYYY HH:mm:ss");
-	const price = formatThousands(item.last * amount, ".");
-	const output = [
-		{
-			title: `${ticker.replace("idr", "/idr").toUpperCase()} - Rp. ${price}`,
-			subtitle: `Last update: ${date} (cmd+c to copy the price)`,
-			arg: `${ticker
-				.replace("idr", "/idr")
-				.toUpperCase()} - Rp. ${price} | Last update: ${date}`,
-			quicklookurl: `https://indodax.com/market/${ticker.toUpperCase()}`,
-			valid: true,
-		},
-	];
-	alfy.output(output);
+  const date = dayjs(item.server_time * 1000).format("DD MMM YYYY HH:mm:ss");
+  const price = formatThousands(item.last * amount, ".");
+  const output = [
+    {
+      title: `${ticker.replace("idr", "/idr").toUpperCase()} - Rp. ${price}`,
+      subtitle: `Last update: ${date} (cmd+c to copy the price)`,
+      arg: `${ticker
+        .replace("idr", "/idr")
+        .toUpperCase()} - Rp. ${price} | Last update: ${date}`,
+      quicklookurl: `https://indodax.com/market/${ticker.toUpperCase()}`,
+      valid: true,
+    },
+  ];
+  alfy.output(output);
 } else {
-	alfy.error(`Pair ${ticker} not found.`);
+  alfy.error(`Pair ${ticker} not found.`);
 }
